d024301d5ec4da6bf045c7157beac8d857b8b7a9
Grace Gude <grace@Graces-MacBook-Pro.local>
2018-12-22 15:59:22 -0600

    Updating files

---
+++ FriendFinder/app/data/friends.js
@@ -0,0 +1,37 @@
+var friendsArray = [
+    {
+        "name":"Ahmed",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+          ]
+      },{
+          "name": "Evan",
+          "photo": "url",
+          "scores": [
+            4,
+            2,
+            5,
+            5,
+            4,
+            1,
+            1,
+            3,
+            5,
+            2
+          ]
+      }
+      
+  ];
+  
+  // Note how we export the array. This makes it accessible to other files using require.
+  module.exports = friendsArray;
